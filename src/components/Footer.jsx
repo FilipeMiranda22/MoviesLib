74deee25b7a2be7f994286d7b2dfade72ff4022d
Filipe Miranda <filipe.salomao22@gmail.com>
2023-06-29 16:01:41 -0300

    Corrigindo caminho do link para o github

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <div>
         <footer className="footer">
             <ul className="social-list">
-                <li><a href='https://github.com/FilipeMiranda22' target="_blank" rel="noreferrer"><FaGithub/></a></li>
+                <li><a href='https://github.com/FilipeMiranda22/MoviesLib' target="_blank" rel="noreferrer"><FaGithub/></a></li>
             </ul>
             <p className="copy-right"><span>MoviesLib</span> &copy; 2023</p>
         </footer>
